371da1d0cf6fdb7f2be0a0619767fa0e10dafe65
quant-1729 <priyanshuagrawal1729@gmail.com>
2024-07-03 16:19:44 +0530

    initial

---
+++ lib/Utils/constants.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColor{
+static const Color Background_blue= Color(0xFF2931a5);
+static const Color Icon_backgorund= Color(0xFFb9d0ff);
+static const Color Screen_background= Color(0xFFededf9);
+
+}
